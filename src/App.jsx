caaa714c80796ee95ee6f9b554da67195e6a5db8
Lizzie King <lizzieking521@gmail.com>
2019-07-10 20:04:42 +1000

    added experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Message from './components/Message/Message';
 import ImageList from './components/ImageList/ImageList';
-import './App.css';
+import Employers from './components/Employers/Employers';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <section className="hero is-fullheight has-background">
+        <section className="hero has-background">
           <img
             className="img hero-background is-transparent"
             src="background1.jpg"
             alt="background of lava"
           />
           <Message> </Message>
-          <ImageList />
+          <Employers></Employers>
         </section>
+        <ImageList />
       </div>
     );
   }
