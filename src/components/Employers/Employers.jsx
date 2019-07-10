65083c90adbcc3edcae26f2fc5402edef5757193
Lizzie King <lizzieking521@gmail.com>
2019-07-10 20:43:09 +1000

    cropped images and changed on hover

--- src/components/Employers/Employers.jsx
+++ src/components/Employers/Employers.jsx
@@ -7,26 +7,26 @@ class Employers extends React.Component {
       <section className="section container">
         <h1 className="title"> Experience </h1>
         <div className="columns is-vcentered">
-          <div className="column has-text-centered is-3">
-            <a className="image">
-              <img className="" src="venuemob.png" alt="venuemob logo" width="50px"/>
-            </a>
-          </div>
-          <div className="column is-3">
-            <a className="image">
-              <img className="" src="myob.png" alt="myob logo" width="50px"/>
-            </a>
-          </div>
-          <div className="column is-3">
-            <a className="image">
+          <a className="column is-3 employers">
+            <div className="image">
+              <img src="venuemob.png" alt="venuemob logo" width="50px"/>
+            </div>
+          </a>
+          <a className="column is-3 employers">
+            <div className="image">
+              <img src="myob.png" alt="myob logo" width="50px"/>
+            </div>
+          </a>
+          <a className="column is-3 employers">
+            <div className="image">
               <img src="deloitte.png" alt="Deloitte logo" width="50px"/>
-            </a>
-          </div>
-          <div className="column is-3">
-            <a className="image">
+            </div>
+          </a>
+          <a className="column is-3 employers">
+            <div className="image">
               <img src="gcomm.webp" alt="GCOMM logo" width="50px"/>
-            </a>
-          </div>
+            </div>
+          </a>
         </div>
       </section>
     );
--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -9,31 +9,30 @@ class ImageList extends React.Component {
         </div>
         <div className="container columns">
           <div className="column is-5">
-            <div className="column is-8">
+            <a className="column is-8">
               <img
                 className="image"
                 src="node.png"
                 alt="node.js logo"
                 width="70px"
               />
-              <br />
+              <br/>
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
-            <div className="column is-8">
+            </a>
+            <a className="column is-8">
               <img
                 className="image"
                 src="es6.svg"
                 alt="es6 logo"
                 width="65px"
               />
-              <br />
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
-            <div className="column is-8">
+            </a>
+            <a className="column is-8">
               <img
                 className="image"
                 src="html.png"
@@ -44,8 +43,8 @@ class ImageList extends React.Component {
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
-            <div className="column is-8">
+            </a>
+            <a className="column is-8">
               <img
                 className="image"
                 src="angular.svg"
@@ -56,10 +55,10 @@ class ImageList extends React.Component {
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
+            </a>
           </div>
           <div className="column is-5">
-            <div className="column is-8">
+            <a className="column is-8">
               <img
                 className="img-logo"
                 src="java.png"
@@ -70,8 +69,8 @@ class ImageList extends React.Component {
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
-            <div className="column is-8">
+            </a>
+            <a className="column is-8">
               <img
                 className="img-logo"
                 src="react.png"
@@ -82,8 +81,8 @@ class ImageList extends React.Component {
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
-            <div className="column is-8">
+            </a>
+            <a className="column is-8">
               <img
                 className="img-logo"
                 src="cplusplus.png"
@@ -94,8 +93,8 @@ class ImageList extends React.Component {
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
-            <div className="column is-8">
+            </a>
+            <a className="column is-8">
               <img
                 className="img-logo"
                 src="js.png"
@@ -106,7 +105,7 @@ class ImageList extends React.Component {
               <progress className="progress is-small" value="80" max="100">
                 45%
               </progress>
-            </div>
+            </a>
           </div>
         </div>
       </section>
